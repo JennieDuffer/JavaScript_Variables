3ab003618d7f8a3b8b4afb763fbd8ee68aa23ce2
Jennie Duffer <jenniej297@gmail.com>
2022-09-14 14:31:00 -0500

    initial commit

--- app.js
+++ app.js
@@ -3,8 +3,33 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
-
-// Exercise 2
+// Exercise 1 & Exercise 2
+const firstName = "Jennie";
+let lastName = "Duffer";
+var age = 30; 
+console.log(`${firstName} ${lastName} is ${age} years old.`);
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+console.log(`${language} was created in ${createdYear}.`);
+
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+console.log(`The shirt you purchased is a size ${selectedSize} and the price is ${price}. 
+The shirt was ${salePercentage}% off, is it still on sale? ${isOnSale}. You purchased the last shirt, so now the stock is down to ${stock}.`);
+ 
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+console.log(`${title} was written by ${author}. It has ${pageCount} pages and you are currently on page ${bookmark}.`);
+
